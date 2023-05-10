3f965c3e60744473d9652b0ede6a04bbef0cfaec
imanolprado <smr22.imanol.prado@gmail.com>
2023-05-10 19:30:30 +0200

    cambio breadcrumb

--- src/components/breadcrumbs/MenuBreadcrumb.jsx
+++ src/components/breadcrumbs/MenuBreadcrumb.jsx
@@ -17,7 +17,7 @@ export default function MenuBreadcrumb() {
   const path = location.pathname.split('/')
 
   return (
-    <Breadcrumb className='bg-slate-100 py-3 px-36  text-slate-600 dark:text-slate-300 dark:bg-slate-500'>
+    <Breadcrumb className='max-md:px-0 bg-slate-100 py-3 px-36  text-slate-600 dark:text-slate-300 dark:bg-slate-500'>
       <BreadcrumbItem icon={HiHome} text='Home' link='/' />
 
       {/* News */}
