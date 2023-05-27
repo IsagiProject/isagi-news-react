0656d80c8c87fc996cc7c84d4a8453557715cc12
Galder315 <315galder@gmail.com>
2023-05-27 12:09:22 +0200

    fix carousel

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,10 @@
 import { Carousel } from '@trendyol-js/react-carousel'
-import { NewsCards } from './NewsCards'
+import { NewsCards } from './NewsCards.jsx'
 
 export function Slider({ news }) {
   return (
-    <Carousel show={3.5} slide={5} className='h-60 py-10'>
-      <NewsCards className='w-4 h-6' />
+    <Carousel show={3.5} slide={3} swiping>
+      <NewsCards />
     </Carousel>
   )
 }
