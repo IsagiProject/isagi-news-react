30185c286eafa1f68be911b54a3def5f6443d140
Galder315 <315galder@gmail.com>
2023-05-29 10:25:08 +0200

    index 3 news and sales

--- src/components/SaleCard.jsx
+++ src/components/SaleCard.jsx
@@ -2,7 +2,7 @@ import { Card } from 'flowbite-react'
 
 export function SaleCard({ sale }) {
   return (
-    <div className='w-1/5 max-lg:w-full'>
+    <div className='w-1/3 max-lg:w-full'>
       <Card className='select-none m-3' imgSrc={sale.sale_id}>
         <h5 className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
           {sale.title}
